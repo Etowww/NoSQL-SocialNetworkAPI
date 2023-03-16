b8b42f24bb71113e7c591397a5565e1b4141f09c
Etowww <etow.engineering@gmail.com>
2023-03-16 13:09:40 -0700

    Created the thought model with the reaction schema / implemented an index.js that imports the two created models / installed the day.js dependency

---
+++ models/thought.js
@@ -0,0 +1,69 @@
+// Start of code [Evan Towlerton]
+
+
+const mongoose = require('mongoose');
+const dayjs = require('dayjs');
+
+// Reaction (SCHEMA ONLY)
+const reactionSchema = new mongoose.Schema({
+    reactionId: {
+        type: mongoose.Types.ObjectId,
+        default: () => new mongoose.Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        minLength: 1,
+        maxLength: 280
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAt => dayjs(createdAt).format('MMM DD, YYYY [at] h:mm:ss a')
+    }
+},
+{
+    toJson: {
+        virtuals: true,
+        getters: true
+    },
+}
+)
+
+
+const thoughtSchema = new mongoose.Schema({
+    thoughtText: {
+        type: String,
+        minLength: 1,
+        maxLength: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAt => dayjs(createdAt).format('MMM DD, YYYY [at] h:mm:ss a')
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    reactions: [reactionSchema]
+},
+{
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+}
+)
+
+const Thought = mongoose.model('thought', thoughtSchema);
+
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+module.exports = {Thought}
